65543f5bb125c526049f81876246545078c7dd18
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-01-27 20:12:30 -0600

    start handlebars

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
+const app = express();
+const expressHandlebars = require('express-handlebars');
 
+app.engine('handlebars', expressHandlebars.engine(
+    {defaultLayout: 'main'}
+));
+
+app.set('view engine', 'handlebars');
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
 
 
 
